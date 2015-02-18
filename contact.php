f377360afe6c06581901e3f7aa69f86d81f1e4ad
Vikas <vikas@sanisoft.com>
2015-02-18 10:05:23 +0530

    Initial Commit

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Page</title>
+</head>
+<body>
+	Name : Demo Name
+	Age : Demo Age 
+	City  : Demo City
+	State : Demo State
+	Address : Demo Address
+</body>
+</html>
